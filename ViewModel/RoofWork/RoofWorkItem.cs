53709608898d85879d20da4bf908431fd809e8d8
adedolapo quasim <dolapo836@gmail.com>
2025-03-08 18:21:54 +0100

    roof work in progress

---
+++ ViewModel/RoofWork/RoofWorkItem.cs
@@ -0,0 +1,131 @@
+﻿using System.Collections.ObjectModel;
+using System.ComponentModel;
+
+namespace ADLMRateGen.ViewModel.RoofWork
+{
+    public class RoofWorkItem: INotifyPropertyChanged
+    {
+		private int _itemNo;
+		private string? _description;
+		private double _netCost;
+		private double _outVal;
+		private double _overheadValue;
+		private double _profitValue;
+		private double _totalCost;
+		private string? _unit;
+		public int ItemNo
+		{
+			get => _itemNo;
+			set
+			{
+				if (_itemNo != value)
+				{
+					_itemNo = value;
+					OnPropertyChanged(nameof(ItemNo));
+				}
+			}
+		}
+		public string Description
+		{
+			get => _description;
+			set
+			{
+				if (_description != value)
+				{
+					_description = value;
+					OnPropertyChanged(nameof(Description));
+				}
+			}
+		}
+		public double NetCost
+		{
+			get => _netCost;
+			set
+			{
+				if (_netCost != value)
+				{
+					_netCost = value;
+					OnPropertyChanged(nameof(NetCost));
+				}
+			}
+		}
+		public double OutVal
+		{
+			get => _outVal;
+			set
+			{
+				if (_outVal != value)
+				{
+					_outVal = value;
+					OnPropertyChanged(nameof(OutVal));
+				}
+			}
+		}
+		public double OverheadValue
+		{
+			get => _overheadValue;
+			set
+			{
+				if (_overheadValue != value)
+				{
+					_overheadValue = value;
+					OnPropertyChanged(nameof(OverheadValue));
+				}
+			}
+		}
+		public double ProfitValue
+		{
+			get => _profitValue;
+			set
+			{
+				if (_profitValue != value)
+				{
+					_profitValue = value;
+					OnPropertyChanged(nameof(ProfitValue));
+				}
+			}
+		}
+		public double TotalCost
+		{
+			get => _totalCost;
+			set
+			{
+				if (_totalCost != value)
+				{
+					_totalCost = value;
+					OnPropertyChanged(nameof(TotalCost));
+				}
+			}
+		}
+		public string Unit
+		{
+			get => _unit;
+			set
+			{
+				if (_unit != value)
+				{
+					_unit = value;
+					OnPropertyChanged(nameof(Unit));
+				}
+			}
+		}
+
+		private ObservableCollection<RoofWorkBreakdownLine> _roofworkBreakdownLines =
+			new ObservableCollection<RoofWorkBreakdownLine>();
+		public ObservableCollection<RoofWorkBreakdownLine> RoofWorkBreakdownLines
+		{
+			get => _roofworkBreakdownLines;
+			set
+			{
+				if (_roofworkBreakdownLines != value)
+				{
+					_roofworkBreakdownLines = value;
+					OnPropertyChanged(nameof(RoofWorkBreakdownLines));
+				}
+			}
+		}
+		public event PropertyChangedEventHandler? PropertyChanged;
+		protected void OnPropertyChanged(string propertyName) =>
+			PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+	}
+}
---
+++ ViewModel/RoofWork/RoofWorkViewModel.cs
@@ -0,0 +1,115 @@
+﻿using System.Collections.ObjectModel;
+using System.ComponentModel;
+using System.Windows.Data;
+using System.Windows.Input;
+using ADLMRateGen.Command;
+using ADLMRateGen.Helpers;
+
+namespace ADLMRateGen.ViewModel.RoofWork
+{
+    public class RoofWorkViewModel: ViewModelBase
+    {
+        private readonly GetItemsFromDB _helper;
+
+        private double _overheadPercent = 10.0;
+        private double _profitPercent = 25.0;
+        private string _searchTerm = string.Empty;
+        private object _selectedDetail;
+
+        public double OverheadPercent
+        {
+            get => _overheadPercent;
+            set
+            {
+                if (_overheadPercent != value)
+                {
+                    _overheadPercent = value;
+                    RaisePropertyChanged();
+                }
+            }
+        }
+        public double ProfitPercent
+        {
+            get => _profitPercent;
+            set
+            {
+                if (_profitPercent != value)
+                {
+                    _profitPercent = value;
+                    RaisePropertyChanged();
+                }
+            }
+        }
+        public ObservableCollection<RoofWorkItem> RoofWorkItems { get; set; } =
+            new ObservableCollection<RoofWorkItem>();
+        public ICollectionView RoofworkCollectionView { get; private set; }
+		public string SearchTerm
+		{
+			get => _searchTerm;
+			set
+			{
+				if (_searchTerm != value)
+				{
+					_searchTerm = value;
+					RaisePropertyChanged();
+					RoofworkCollectionView.Refresh();
+				}
+			}
+		}
+		public object SelectedDetail
+		{
+			get => _selectedDetail;
+			set
+			{
+				if (_selectedDetail != value)
+				{
+					_selectedDetail = value;
+					RaisePropertyChanged();
+				}
+			}
+		}
+		public ICommand RecomputeCommand { get; }
+		public ICommand ShowDetailsCommand { get; }
+        public RoofWorkViewModel(MaterialLibraryViewModel matLib, LabourLibraryViewModel labourLib)
+        {
+            _helper = new GetItemsFromDB(matLib, labourLib);
+            matLib.LibraryChanged += OnLibraryChange;
+            labourLib.LibraryChanged += OnLibraryChange;
+
+            BuildRoofworkItem();
+
+            RoofworkCollectionView = CollectionViewSource.GetDefaultView(RoofWorkItems);
+            RoofworkCollectionView.Filter = FilterRoofItem;
+
+			RecomputeCommand = new DelegateCommand(o => RecomputeAll());
+            ShowDetailsCommand = new DelegateCommand(o => ShowDetails(o));
+        }
+
+		#region Function Method
+		private void ShowDetails(object o)
+		{
+			throw new NotImplementedException();
+		}
+
+		private void RecomputeAll()
+		{
+			throw new NotImplementedException();
+		}
+
+		private bool FilterRoofItem(object obj)
+		{
+			throw new NotImplementedException();
+		}
+
+		private void BuildRoofworkItem()
+		{
+			throw new NotImplementedException();
+		}
+
+		private void OnLibraryChange()
+		{
+			throw new NotImplementedException();
+		}
+		#endregion
+	}
+}
