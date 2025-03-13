d4a8370dbb5ee783f05810f4aa0372c46f812703
damiPC <User@DESKTOP-3PQA4MC>
2025-03-13 09:58:34 +0100

    paintork updated

--- ViewModel/Painting/PaintWorkViewModel.cs
+++ ViewModel/Painting/PaintWorkViewModel.cs
@@ -1,5 +1,6 @@
 ﻿using System.Collections.ObjectModel;
 using System.ComponentModel;
+using System.Windows;
 using System.Windows.Data;
 using System.Windows.Input;
 using ADLMRateGen.Command;
@@ -147,7 +148,8 @@ namespace ADLMRateGen.ViewModel.Painting
         {
             Func<PaintWorkItem>[] computeMethods =
             {
-                //ComputeItem1,ComputeItem2,ComputeItem3,ComputeItem4,ComputeItem5,ComputeItem6,ComputeItem7,
+                ComputeItem1, ComputeItem2, ComputeItem3,
+                //ComputeItem4,ComputeItem5,ComputeItem6,ComputeItem7,
                 //ComputeItem8,ComputeItem9,ComputeItem10,ComputeItem11,ComputeItem12
             };
 
@@ -161,17 +163,233 @@ namespace ADLMRateGen.ViewModel.Painting
         #region COMPUTE METHOD
         private PaintWorkItem ComputeItem1()
         {
-            throw new NotImplementedException();
-        }
+            //MATERIAL CLASS
+            double puttyCost = GetMaterialPrice("Poly Filla 1.8Kg Pack.");
+            double antiFungalCost = GetMaterialPrice("Pealux Anti Fungi/Algae Emulsion")/4;
+            double texcoteCost = GetMaterialPrice("Peacotex Textured Finish (B/W)") /25;
 
+            double puttyQty = 0.3;
+            double antiFungalQty = 0.4;
+            double texcoteQty = 1;
+
+            double puttyRate = puttyCost * puttyQty;
+            double antiFungalRate = antiFungalCost * antiFungalQty;
+            double texcoteRate = texcoteCost * texcoteQty;
+
+            double puttyWastePer = 2.5;
+            double antiFungalWastePer = 10;
+            double texcoatWastePer = 10;
+
+            double puttyWaste = puttyRate * (puttyWastePer / 100);
+            double antiFungalWaste = antiFungalRate *(antiFungalWastePer / 100);
+            double texcoatWaste = texcoteRate * (texcoatWastePer / 100);
+
+            double materialTotal = puttyRate + antiFungalRate + texcoteRate + 
+                puttyWaste + antiFungalWaste + texcoatWaste;
+
+            //LABOUR COST
+			double painterCost = (GetLabourRate("Skilled/Artisan") ) * 1.4;
+            double painterQty = 2;
+            double painterRate = painterCost * painterQty;
+
+            double painterOutput = 18;
+
+            double painterOutputPerSqm = painterRate / painterOutput;
+
+            double netCostPerSqm = materialTotal + painterOutputPerSqm;
+            var ohp = ApplyOHP(netCostPerSqm);
+
+            var breakdown = new ObservableCollection<PaintingBreakdownLine>
+            {
+                new PaintingBreakdownLine{ ComponentName="Putty filler to joints and fine cracks", Quantity=puttyQty, Unit="kg/m2",
+                    UnitPrice= puttyCost, TotalPrice=puttyRate},
+                new PaintingBreakdownLine{ComponentName="Add waste", Quantity=puttyWastePer, Unit="%",
+                    TotalPrice=puttyWaste},
+                new PaintingBreakdownLine{ ComponentName="Anti-fungal paint", Quantity=antiFungalQty, Unit="Lit/m2",
+                    UnitPrice= antiFungalCost, TotalPrice=antiFungalRate},
+                new PaintingBreakdownLine{ComponentName="Add waste", Quantity=antiFungalWastePer, Unit="%",
+                    TotalPrice=antiFungalWaste},
+                new PaintingBreakdownLine{ ComponentName="Texcote-(Pealux - 25kg drum)", Quantity=texcoteQty, Unit="kg/m2",
+                    UnitPrice= texcoteCost, TotalPrice=texcoteRate},
+                new PaintingBreakdownLine{ComponentName="Add waste", Quantity=texcoatWastePer, Unit="%",
+                    TotalPrice=texcoatWaste},
+                new PaintingBreakdownLine{ComponentName="Total Material", TotalPrice=materialTotal},
+
+                new PaintingBreakdownLine{ComponentName="Painter", Quantity=painterQty, Unit="per/day", UnitPrice=painterCost,
+                    TotalPrice=painterRate},
+                new PaintingBreakdownLine{ComponentName="Output", Quantity=painterOutput, Unit="m2/day", 
+                    UnitPrice=painterRate, TotalPrice= painterOutputPerSqm},
+
+
+                new PaintingBreakdownLine{ComponentName="Total Cost per m2", Unit="m2", TotalPrice=netCostPerSqm}
+            };
+
+            return new PaintWorkItem
+            {
+                ItemNo = 1,
+                Description = "Prepare and apply one undercoat anti-fungal paint and " +
+                "one finish coat white texcote (peacock) paint to wall not exceeding " +
+                "4.00m from ground level, on blockwork or concrete externally.",
+                Unit = "M2",
+                NetCost = Math.Round(netCostPerSqm, 2),
+                OverheadValue = Math.Round(ohp.overheadVal, 2),
+                ProfitValue = Math.Round(ohp.profitVal, 2),
+                TotalCost = Math.Round(ohp.total, 2),
+                PaintingBreakdownLines = breakdown
+            };
+
+        }
         private PaintWorkItem ComputeItem2()
         {
-            throw new NotImplementedException();
-        }
+            //MATERIAL CLASS
+            double puttyCost = GetMaterialPrice("Poly Filla 1.8Kg Pack.");
+            //double antiFungalCost = GetMaterialPrice("Pealux Anti Fungi/Algae Emulsion") / 4;
+            double texcoteCost = GetMaterialPrice("Peacotex Textured Finish (B/W)") / 25;
 
+            double puttyQty = 0.3;
+            //double antiFungalQty = 0.4;
+            double texcoteQty = 1;
+
+            double puttyRate = puttyCost * puttyQty;
+            //double antiFungalRate = antiFungalCost * antiFungalQty;
+            double texcoteRate = texcoteCost * texcoteQty;
+
+            double puttyWastePer = 2.5;
+            //double antiFungalWastePer = 10;
+            double texcoatWastePer = 10;
+
+            double puttyWaste = puttyRate * (puttyWastePer / 100);
+            //double antiFungalWaste = antiFungalRate * (antiFungalWastePer / 100);
+            double texcoatWaste = texcoteRate * (texcoatWastePer / 100);
+
+            double materialTotal = puttyRate  + texcoteRate +
+                puttyWaste  + texcoatWaste;
+
+            //LABOUR COST
+            double painterCost = (GetLabourRate("Skilled/Artisan")) * 1.4;
+            double painterQty = 2;
+            double painterRate = painterCost * painterQty;
+
+            double painterOutput = 30;
+
+            double painterOutputPerSqm = painterRate / painterOutput;
+
+            double netCostPerSqm = materialTotal + painterOutputPerSqm;
+            var ohp = ApplyOHP(netCostPerSqm);
+
+            var breakdown = new ObservableCollection<PaintingBreakdownLine>
+            {
+                new PaintingBreakdownLine{ ComponentName="Putty filler to joints and fine cracks", Quantity=puttyQty, Unit="kg/m2",
+                    UnitPrice= puttyCost, TotalPrice=puttyRate},
+                new PaintingBreakdownLine{ComponentName="Add waste", Quantity=puttyWastePer, Unit="%",
+                    TotalPrice=puttyWaste},
+                //new PaintingBreakdownLine{ ComponentName="Anti-fungal paint", Quantity=antiFungalQty, Unit="Lit/m2",
+                //    UnitPrice= antiFungalCost, TotalPrice=antiFungalRate},
+                //new PaintingBreakdownLine{ComponentName="Add waste", Quantity=antiFungalWastePer, Unit="%",
+                //    TotalPrice=antiFungalWaste},
+                new PaintingBreakdownLine{ ComponentName="Texcote-(Pealux - 25kg drum)", Quantity=texcoteQty, Unit="kg/m2",
+                    UnitPrice= texcoteCost, TotalPrice=texcoteRate},
+                new PaintingBreakdownLine{ComponentName="Add waste", Quantity=texcoatWastePer, Unit="%",
+                    TotalPrice=texcoatWaste},
+                new PaintingBreakdownLine{ComponentName="Total Material", TotalPrice=materialTotal},
+
+                new PaintingBreakdownLine{ComponentName="Painter", Quantity=painterQty, Unit="per/day", UnitPrice=painterCost,
+                    TotalPrice=painterRate},
+                new PaintingBreakdownLine{ComponentName="Output", Quantity=painterOutput, Unit="m2/day",
+                    UnitPrice=painterRate, TotalPrice= painterOutputPerSqm},
+
+
+                new PaintingBreakdownLine{ComponentName="Total Cost per m2", Unit="m2", TotalPrice=netCostPerSqm}
+            };
+
+            return new PaintWorkItem
+            {
+                ItemNo = 2,
+                Description = "Prepare and apply white texcote (peacock) paint to wall " +
+                "not exceeding 4.00m from ground level internally.",
+                Unit = "M2",
+                NetCost = Math.Round(netCostPerSqm, 2),
+                OverheadValue = Math.Round(ohp.overheadVal, 2),
+                ProfitValue = Math.Round(ohp.profitVal, 2),
+                TotalCost = Math.Round(ohp.total, 2),
+                PaintingBreakdownLines = breakdown
+            };
+
+        }
         private PaintWorkItem ComputeItem3()
         {
-            throw new NotImplementedException();
+            //MATERIAL CLASS
+            double puttyCost = GetMaterialPrice("Poly Filla 1.8Kg Pack.");
+            double primingCost = GetMaterialPrice("White Emulsion (High Quality)") / 4;
+            double emulsionCost = GetMaterialPrice("White Emulsion (High Quality)") / 4;
+
+            double puttyQty = 0.3;
+            double primingQty = 0.28;
+            double emulsionQty = 0.42;
+
+            double puttyRate = puttyCost * puttyQty;
+            double primingRate = primingCost * primingQty;
+            double emulsionRate = emulsionCost * emulsionQty;
+
+            double puttyWastePer = 2.5;
+            double primingWastePer = 10;
+            double emulsionWastePer = 10;
+
+            double puttyWaste = puttyRate * (puttyWastePer / 100);
+            double primingWaste = primingRate * (primingWastePer / 100);
+            double emulsionWaste = emulsionRate * (emulsionWastePer / 100);
+
+            double materialTotal = puttyRate + primingRate + emulsionRate +
+                puttyWaste + primingWaste + emulsionWaste;
+
+            //LABOUR COST
+            double painterCost = (GetLabourRate("Skilled/Artisan")) * 1.4;
+            double painterQty = 2;
+            double painterRate = painterCost * painterQty;
+
+            double painterOutput = 20;
+
+            double painterOutputPerSqm = painterRate / painterOutput;
+
+            double netCostPerSqm = materialTotal + painterOutputPerSqm;
+            var ohp = ApplyOHP(netCostPerSqm);
+
+            var breakdown = new ObservableCollection<PaintingBreakdownLine>
+            {
+                new PaintingBreakdownLine{ ComponentName="Putty filler to joints and fine cracks", Quantity=puttyQty, Unit="kg/m2",
+                    UnitPrice= puttyCost, TotalPrice=puttyRate},
+                new PaintingBreakdownLine{ComponentName="Add waste", Quantity=puttyWastePer, Unit="%",
+                    TotalPrice=puttyWaste},
+                new PaintingBreakdownLine{ ComponentName="Priming paint", Quantity=primingQty, Unit="Lit/m2",
+                    UnitPrice= primingCost, TotalPrice=primingRate},
+                new PaintingBreakdownLine{ComponentName="Add waste", Quantity=primingWastePer, Unit="%",
+                    TotalPrice=primingWaste},
+                new PaintingBreakdownLine{ ComponentName="Emulsion Paint - (Peacock)", Quantity=emulsionQty, Unit="Lit/m2",
+                    UnitPrice= emulsionCost, TotalPrice=emulsionRate},
+                new PaintingBreakdownLine{ComponentName="Add waste", Quantity=emulsionWastePer, Unit="%",
+                    TotalPrice=emulsionWaste},
+                new PaintingBreakdownLine{ComponentName="Total Material", TotalPrice=materialTotal},
+
+                new PaintingBreakdownLine{ComponentName="Painter", Quantity=painterQty, Unit="per/day", UnitPrice=painterCost,
+                    TotalPrice=painterRate},
+                new PaintingBreakdownLine{ComponentName="Output", Quantity=painterOutput, Unit="m2/day",
+                    UnitPrice=painterRate, TotalPrice= painterOutputPerSqm},
+
+
+                new PaintingBreakdownLine{ComponentName="Total Cost per m2", Unit="m2", TotalPrice=netCostPerSqm}
+            };
+
+            return new PaintWorkItem
+            {
+                ItemNo = 3,
+                Description = "Prepare and apply one undercoat and two finish coats quality, white emulsion  paint, to wall not exceeding 4.00m from ground level internally.",
+                Unit = "M2",
+                NetCost = Math.Round(netCostPerSqm, 2),
+                OverheadValue = Math.Round(ohp.overheadVal, 2),
+                ProfitValue = Math.Round(ohp.profitVal, 2),
+                TotalCost = Math.Round(ohp.total, 2),
+                PaintingBreakdownLines = breakdown
+            };
         }
 
         private PaintWorkItem ComputeItem4()
